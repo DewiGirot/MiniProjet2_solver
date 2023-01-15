ba5db59772073af389de15311dd0e0af8d7e4183
Déwi <72131554+DewiGirot@users.noreply.github.com>
2023-01-15 22:41:28 +0000

    Added the files given with the subject

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
