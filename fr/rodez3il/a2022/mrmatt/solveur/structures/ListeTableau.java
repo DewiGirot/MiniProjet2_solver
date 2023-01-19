fa2b86d5ea7e8fa99642b51b76798aaced71b799
Dewi <dewi.girot@gmail.com>
2023-01-19 23:37:39 +0100

    Update ListeTableau.java

--- fr/rodez3il/a2022/mrmatt/solveur/structures/ListeTableau.java
+++ fr/rodez3il/a2022/mrmatt/solveur/structures/ListeTableau.java
@@ -1,18 +1,48 @@
 package fr.rodez3il.a2022.mrmatt.solveur.structures;
-
 import java.util.Arrays;
 
-public class ListeTableau<E> implements Liste<E> {
+public class ListeTableau<T> implements Liste<T> {
 
-  public void ajouter(T element);
+  private ArrayList<T> tab;
+  private int size = 0;
 
-  public boolean estVide();
+  public ListeTableau(int size) throws Exception {
+    if(size < 0){
+      throw new Exception('Vous ne pouvez pas indiquer une taille inférieure à 0');
+    }
+    if(size == null)(
+      size = 0;
+    )
+    this.tab = new ArrayList<T>()[size];
+    this.size = size;
+  }
 
-  public int taille();
+  public void ajouter(T element) throws Exception {
+    if(this.tab.size == this.size) {
+      throw new Exception('Taille du tableau maximal atteint');
+    }
+    this.tab.add(element);
+  }
 
-  public T enlever(int i);
+  public boolean estVide(){
+    return this.tab.size == 0 ? true : false;
+  }
 
-  public T element(int i);
+  public int taille(){
+    return this.size;
+  }
 
-  public boolean contient(T e);
+  public T enlever(int i){
+    T tmp = this.tab[i];
+    this.tab.remove(i);
+    return tmp:
+  }
+
+  public T element(int i){
+    return this.tab[i];
+  }
+
+  public boolean contient(T e){
+    return this.tab.contains(e);
+  }
 }
