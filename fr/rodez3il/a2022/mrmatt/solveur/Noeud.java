cc274ceb2ba853f4a7395cb01c127bee0109d8c7
Déwi <72131554+DewiGirot@users.noreply.github.com>
2023-02-06 22:48:41 +0000

    Update Noeud -> Don't understand visiter() Method calculerFils on going.

--- fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
+++ fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
@@ -8,26 +8,44 @@ public class Noeud {
 
   private DictionnaireChaine<Integer, String> configuration;
   private Niveau etatNiveau;
-  private int[] fils;
+  private String[] fils;
   private ListeChainee<String> commandes;
 
-  public Noeud(Niveau etatNiveau) {
+  public Noeud(DictionnaireChaine<Integer, String> configuration, Niveau etatNiveau, ListeChainee<String> commandes) {
+    this.configuration = configuration;
     this.etatNiveau = etatNiveau;
-    this.fils = new int[4];
-    this.configuration = new DictionnaireChaine<>();
+    this.fils = new String[4];
+    this.commandes = commandes;
   }
 
-  public void visiter() {
-    // getter
+  public String[] getFils() {
+    return this.fils;
+  }
+
+  // ToDo
+  public String visiter() {
+    return this.etatNiveau.toString();
   }
 
   public void calculerFils() {
-    // ToDo
+    if (visiter() != null) {
+      configuration.inserer(this.fils.length, etatNiveau.toString());
+      for (int i = 0; i < 4; i++) {
+        // ToDo
+      }
+    }
   }
 
   @Override
-  public boolean equals(Object o) {
-    return this.etatNiveau.equals(o);
+  public boolean equals(Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof Noeud)) {
+      return false;
+    }
+    Noeud other = (Noeud) obj;
+    return this.etatNiveau.toString().equals(other.etatNiveau.toString());
   }
 
   @Override
